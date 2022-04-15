eb781004378978eb32606f27cd9f5d7619929ab2
Tony <awalker780@gmail.com>
2022-04-15 10:29:12 -0400

    Implements admin dashboard show page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,5 +5,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: user_name, password: user_password
   
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
+
 end
