f10366b0c6e84b73c3981652238aaaaa3de98571
Tony <awalker780@gmail.com>
2022-04-15 11:19:12 -0400

    Create about controller via rails generator

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,8 @@
+class AboutController < ApplicationController
+
+  def index
+    @about_us_message = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium officia assumenda non ipsam illum, reprehenderit dignissimos nam suscipit mollitia. Ex facere optio ab aspernatur nihil in ullam repudiandae voluptatibus iste!"
+  end
+
+end
+
