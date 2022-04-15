0db0a339b7bdde8f22e8b4d8a34a9f268090daad
Tony <awalker780@gmail.com>
2022-04-15 14:37:32 -0400

    Create controller and views via rails controller generator

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,33 @@
+class Admin::CategoriesController < ApplicationController
+  user_name = ENV['USER_NAME']
+  user_password = ENV['USER_PASSWORD']
+
+  http_basic_authenticate_with name: user_name, password: user_password
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @categories = Category.new
+  end
+
+  def create
+    @categories = Category.new(category_params)
+
+    if @categories.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
+end
