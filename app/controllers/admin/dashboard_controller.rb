09e566c2d3144286deef0d77f5cdb861fc9e3789
Tony <awalker780@gmail.com>
2022-04-14 20:51:17 -0400

    Implements HTTP authentication for admin section of site

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
+  user_name = ENV['USER_NAME']
+  user_password = ENV['USER_PASSWORD']
+
+  http_basic_authenticate_with name: user_name, password: user_password
+  
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,8 @@
 class Admin::ProductsController < ApplicationController
+  user_name = ENV['USER_NAME']
+  user_password = ENV['USER_PASSWORD']
+
+  http_basic_authenticate_with name: user_name, password: user_password
 
   def index
     @products = Product.order(id: :desc).all
