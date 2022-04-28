1c2634fa112d527c556e6d20b9c420399c389f0b
Tony <awalker780@gmail.com>
2022-04-27 22:48:17 -0400

    Add more validations, scope, and a class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,24 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates :password, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 5 }
   validates :password_confirmation, presence: true
+
+  scope :ci_find, lambda { |attribute, value| where("lower(#{attribute}) = ?", value.downcase).first }
+
+  def self.authenticate_with_credentials(email, password)
+
+    email_trimmed = email.strip
+
+    user = User.ci_find("email", email_trimmed)
+
+    if user && user.authenticate(password) 
+      user
+    else
+      nil
+    end
+    
+  end
+  
 end
